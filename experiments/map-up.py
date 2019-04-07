9a227d3ccde48a7b2012a9b77b936a23bc7edb10
tkzv <tkzv@users.noreply.github.com>
2019-04-08 01:10:52 +0300

    Linear congruential generator for choice

--- experiments/map-up.py
+++ experiments/map-up.py
@@ -1,15 +1,23 @@
 #!/usr/bin/python3
 import numpy as np
 from PIL import Image
-import random
+
+def fast_random( state ):
+    state = state * 1664525 + 1013904223
+    return state & 0x7fff
+
+def fast_choice( state, sequence ): # в 3.9 раз быстрее random.choice()
+    N = len( sequence )
+    #value = (((state * 1664525 + 1013904223) & 0x7fff) * N) // 0x8000 # на 8% медленнее
+    value = (((state * 1664525 + 1013904223) & 0x7fff) * N) >> 15
+    #value = int((((state * 1664525 + 1013904223) & 0x7fff) * N) / 0x8000) # на 32% медленнее
+    return sequence[value]
 
 ar=[None]*8
-#ar[0] = np.asarray(Image.open('down1.png'))
-ar[0] = np.asarray(Image.open('down2.png'))
-#random.seed(1)
+ar[0] = np.asarray(Image.open('down1.png'))
+#ar[0] = np.asarray(Image.open('down2.png'))
 
 for p2 in range(1,8): #8
-  #random.seed(p2)
   ar[p2] = np.zeros( (ar[p2-1].shape[0]*2, ar[p2-1].shape[1]*2, ar[p2-1].shape[2] ), np.uint8 )
   # оригинал
   for x in   range(ar[p2-1].shape[0]):
@@ -19,26 +27,23 @@ for p2 in range(1,8): #8
   # центр
   for x in   range(1, maxx, 2):
     for y in range(1, maxy, 2):
-      random.seed( x+y*maxy )
-      ar[p2][x,y] = random.choice( (ar[p2][(x-1)%maxx,(y-1)%maxy], 
-                                    ar[p2][(x+1)%maxx,(y-1)%maxy], 
-                                    ar[p2][(x-1)%maxx,(y+1)%maxy], 
-                                    ar[p2][(x+1)%maxx,(y+1)%maxy]) )
+      ar[p2][x,y] = fast_choice( x+y*maxy, (ar[p2][(x-1)%maxx,(y-1)%maxy], 
+                                            ar[p2][(x+1)%maxx,(y-1)%maxy], 
+                                            ar[p2][(x-1)%maxx,(y+1)%maxy], 
+                                            ar[p2][(x+1)%maxx,(y+1)%maxy]) )
   # вертикальные рёбра
   for x in   range(0, maxx, 2):
     for y in range(1, maxy, 2):
-      random.seed( x+y*maxy )
-      ar[p2][x,y] = random.choice( (ar[p2][(x-1)%maxx,y], 
-                                    ar[p2][(x+1)%maxx,y], 
-                                    ar[p2][x,(y-1)%maxy], 
-                                    ar[p2][x,(y+1)%maxy]) )
+      ar[p2][x,y] = fast_choice( x+y*maxy, (ar[p2][(x-1)%maxx,y], 
+                                            ar[p2][(x+1)%maxx,y], 
+                                            ar[p2][x,(y-1)%maxy], 
+                                            ar[p2][x,(y+1)%maxy]) )
   # горизонтальные рёбра
   for x in   range(1, maxx, 2):
     for y in range(0, maxy, 2):
-      random.seed( x+y*maxy )
-      ar[p2][x,y] = random.choice( (ar[p2][(x-1)%maxx,y], 
-                                    ar[p2][(x+1)%maxx,y], 
-                                    ar[p2][x,(y-1)%maxy], 
-                                    ar[p2][x,(y+1)%maxy]) )
-  #Image.fromarray( ar[p2], 'RGB' ).save( 'up-jan-' + str(p2) + '.png' )
-  Image.fromarray( ar[p2], 'RGB' ).save( 'up-jun-' + str(p2) + '.png' )
+      ar[p2][x,y] = fast_choice( x+y*maxy, (ar[p2][(x-1)%maxx,y], 
+                                            ar[p2][(x+1)%maxx,y], 
+                                            ar[p2][x,(y-1)%maxy], 
+                                            ar[p2][x,(y+1)%maxy]) )
+  Image.fromarray( ar[p2], 'RGB' ).save( 'up-jan-' + str(p2) + '.png' )
+  #Image.fromarray( ar[p2], 'RGB' ).save( 'up-jun-' + str(p2) + '.png' )
